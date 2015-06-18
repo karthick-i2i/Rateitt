43e2a21ef70638aca8c981f94e7f702c775081d4
Karthick.M <karthick.m@ideas2it.com>
2015-06-18 17:20:09 +0530

    social config

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '113491855653248',
+    secret: 'cc5a1ac2fbc475785ff59bcca204c0ca'
+});
